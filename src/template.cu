42744761fda7844953362fef7756739bed325cee
marceauclavel <clavelmarceau@gmail.com>
2021-03-29 19:07:48 +0200

    intitialize GPU memory

--- src/template.cu
+++ src/template.cu
@@ -1,42 +1,129 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <time.h>
+
 #include <cuda.h>
 #include <cuda_runtime.h>
 
-__global__ void add( int *a, int *b, int *c ) {
-	*c = *a + *b;
+#define ASCIIMIN 32
+#define ASCIIMAX 126
+
+int isValid(char* c){
+	int asciicode = (int)*c;
+	int valid = (asciicode <= 126);
+	valid = valid && (asciicode >= 32);
+	return valid;
 }
 
-int main() {
-	int a, b, c; // host copies of a, b, c
-	int *dev_a, *dev_b, *dev_c; // device copies of a, b, c
-	int size = sizeof( int ); // we need space for an integer
+int main(int argc, char** argv){
 
-	// allocate device copies of a, b, c
-	cudaMalloc( (void**)&dev_a, size );
-	cudaMalloc( (void**)&dev_b, size );
-	cudaMalloc( (void**)&dev_c, size );
-	a = 2;
-	b = 7;
+	clock_t t1, t2;
+	printf("Initialisation...\n");
+	t1 = clock();
 
-	// copy inputs to device
-	cudaMemcpy( dev_a, &a, size, cudaMemcpyHostToDevice );
-	cudaMemcpy( dev_b, &b, size, cudaMemcpyHostToDevice );
+	//Declarations
+	FILE* inputFile =  NULL;
+	FILE* outputFile =  NULL;
+	char* inputFileName = NULL;
+	char* outputFileName = NULL;
 
-	// launch add() kernel on GPU, passing parameters
-	add<<< 1, 1 >>>( dev_a, dev_b, dev_c );
+	int nChars = 0;
+	char* chars;
+	int nCounts = ASCIIMAX - ASCIIMIN + 1;
+	int* counts;
 
-	// copy device result back to host copy of c
-	cudaMemcpy( &c, dev_c, size, cudaMemcpyDeviceToHost );
+	char* dev_chars;
+	int* dev_counts;
 
-	if (c == 9) {
-		printf("SUCCESS\n");
-	} else {
-		printf("FAILURE\n");
+	int opt;
+
+	//Get comand line options
+	while ((opt = getopt (argc, argv, "i:o:")) != -1) {
+		switch(opt) {
+			case 'i':
+				inputFileName = optarg;
+				break;
+			case 'o':
+				outputFileName = optarg;
+				break;
+		}
 	}
 
-	cudaFree( dev_a );
-	cudaFree( dev_b );
-	cudaFree( dev_c );
+	//Count number of chars in inputFile
+	inputFile = fopen(inputFileName,"r");
+	if (!inputFile) return 1;
+	nChars = 0;
+	for (char c = getc(inputFile); c != EOF; c = getc(inputFile)){
+		if(isValid(&c)) ++nChars;
+	}
+	fclose(inputFile);
+
+	//Allocate memory
+	counts = (int*) malloc(nCounts * sizeof(int));
+	chars = (char*) malloc(nChars * sizeof(char));
+	if(chars == NULL) {
+		printf("Input file too large!\n");
+		return 1;
+	}
+	printf("%d chars processed\n", nChars);
+	cudaMalloc( (void**)&dev_chars, nChars * sizeof(char));
+	cudaMalloc( (void**)&dev_counts, nCounts * sizeof(int));
+
+	//Filling chars array
+	inputFile = fopen(inputFileName,"r");
+	if (!inputFile) return 1;
+	int i = 0;
+	for (char c = getc(inputFile); c != EOF; c = getc(inputFile)){
+		if(isValid(&c)){
+			if (c>=65 && c<=90) chars[i] = (char)(c + 32);
+			else chars[i] = c;
+			++i;
+		}
+	}
+	fclose(inputFile);
+
+	t1 = clock() - t1;
+	printf("Process...\n");
+	t2 = clock();
+
+	//Initialize counter array
+	for (int i = 0; i < nCounts; ++i){
+		counts[i] = 0;
+	}
+
+	cudaMemcpy(dev_chars, chars, nChars * sizeof(char), cudaMemcpyHostToDevice);
+	cudaMemcpy(dev_counts, counts, nCounts * sizeof(int), cudaMemcpyHostToDevice);
+
+	//Count chars
+	for (int i = 0; i < nChars; ++i){
+		int ascii = (int)chars[i];
+		++counts[ascii - ASCIIMIN];
+	}
+
+	t2 = clock() - t2;
+
+	//Write in outputFile
+	outputFile = fopen(outputFileName, "w+");
+	if (!outputFile) return 1;
+	for (int i = 0; i < 127 - ASCIIMIN; ++i){
+		if (i + ASCIIMIN < 65 || i + ASCIIMIN > 90) {
+			fprintf(outputFile, "%c:%d\n", (char)(i + ASCIIMIN), (int)counts[i]);
+		}
+	}
+	fclose(outputFile);
+
+	//Return memory
+	cudaFree(dev_chars);
+	cudaFree(dev_counts);
+	free(chars);
+	free(counts);
+
+	printf("Timings:\nInitialisation: %f\nProcess:        %f\n", (float)1000 * t1/CLOCKS_PER_SEC, (float)1000 * t2/CLOCKS_PER_SEC);
+
 
 	return 0;
+
 }
+
